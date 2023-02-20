88f7bdba3ecde9d0e79ecd45989c03792b43a51e
proballstsar <rohanf6219@gmail.com>
2023-02-19 20:56:48 -0800

    [Media] Get screenshare to work + download on dis

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,75 +1,64 @@
 import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
-import Image from "next/image";
-import reactLogo from "../assets/react.svg";
-import tauriLogo from "../assets/tauri.svg";
-import nextLogo from "../assets/next.svg";
+
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [name, setName] = useState<string>("");
+  const [media, setMedia] = useState<MediaRecorder>(null);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  const [anchor, setAnchor] = useState(<></>);
+
+  async function a() {
+    let videoStream = await navigator.mediaDevices.getDisplayMedia({
+      video: {
+        displaySurface: "monitor",
+        cursor: "always",
+        width: { ideal: 1920, max: 1920 },
+    height: { ideal: 1080, max: 1080 }
+      }
+    })
+
+    const chunks = []
+
+    setMedia(() => {
+      const m: MediaRecorder = new MediaRecorder(videoStream)
+      console.log(m)
+      console.log("START")
+      m.ondataavailable = e => {
+        console.log(e);
+        chunks.push(e.data)
+      }
+      m.onstop = (ev) => {
+        // window.clearTimeout(timoutStop);
+        let blob = new Blob(chunks, { type: "video/mp4" });
+        console.log(blob)
+        videoStream.getTracks().forEach(function (track) {
+            track.stop();
+        });
+        var url = URL.createObjectURL(blob);
+        setAnchor(<a href={url} download="test.mp4">
+          Download
+        </a>)
+
+    }
+      m.start(10);
+      return m;
+    })
   }
 
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <span className="logos">
-          <a href="https://nextjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={nextLogo}
-              className="logo next"
-              alt="Next logo"
-            />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://tauri.app" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={tauriLogo}
-              className="logo tauri"
-              alt="Tauri logo"
-            />
-          </a>
-        </span>
-        <span className="logos">
-          <a href="https://reactjs.org" target="_blank">
-            <Image
-              width={144}
-              height={144}
-              src={reactLogo}
-              className="logo react"
-              alt="React logo"
-            />
-          </a>
-        </span>
-      </div>
-
-      <p>Click on the Tauri, Next, and React logos to learn more.</p>
-
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
-      </div>
-
-      <p>{greetMsg}</p>
+      <button
+        onClick={() => a()}
+      >
+        Connect
+      </button>
+      <button
+        onClick={() => media.stop()}
+      >
+        Disconnected
+      </button>
+      {anchor}
     </div>
   );
 }
